a33a8cb45860935a3533e01bc536c03e0c0d128a
Linka Sofia <linkasofialunkes@gmail.com>
2025-10-06 10:35:30 -0300

    Fix: Adicionar Cache-Control e Pragma aos headers permitidos no CORS

--- server/routes.ts
+++ server/routes.ts
@@ -34,7 +34,7 @@ export async function registerRoutes(app: Express): Promise<Server> {
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie']
+    allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Cache-Control', 'Pragma']
   }));
 
   // Servir arquivos estáticos do diretório public
